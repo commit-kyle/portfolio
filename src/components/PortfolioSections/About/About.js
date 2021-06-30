1d7c8158695ce5b038e4e017946aa4e6b9a273be
Kyle Buttress <kylebuttress11@gmail.com>
2021-06-30 15:14:49 +0200

    Imported AboutImage component

--- src/components/PortfolioSections/About/About.js
+++ src/components/PortfolioSections/About/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './About.module.css';
 
 import Info from '../About/AboutInfo/AboutInfo';
+import AboutImage from './AboutImage/AboutImage';
 
 const About = props => {
 	const aboutData = {
@@ -10,34 +11,33 @@ const About = props => {
 			header: 'work hard',
 			subHeader:
 				"To work hard means to push yourself further and that's what you need to swim across the atlantic",
-			text:
-				"I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
+			text: "I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
 		},
 
 		second: {
 			header: 'work hard',
 			subHeader:
 				"To work hard means to push yourself further and that's what you need to swim across the atlantic",
-			text:
-				"I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
+			text: "I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
 		},
 
 		third: {
 			header: 'work hard',
 			subHeader:
 				"To work hard means to push yourself further and that's what you need to swim across the atlantic",
-			text:
-				"I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
+			text: "I've been working hard all my life and I've even been doing so in school and when I come across a sucker I lick it so that it doesn't drip into the land of the thirsty and I don't know what I'm writing but I hope it all makes sense to the dogs who lie in the lounge when they dream about donkeys",
 		},
 	};
 
 	return (
 		<section id="About" className={classes.Child}>
 			<div className={classes.Container}>
-				<div className={classes.Header}>
-					<h2 className={classes.Heading}>A little about me</h2>
+				<div className={classes.HeaderContainer}>
+					<div className={classes.Header}>
+						<h2 className={classes.Heading}>A little about me</h2>
+					</div>
+					<AboutImage/>
 				</div>
-
 				<div className={classes.Parent}>
 					<Info
 						number={'1'}
