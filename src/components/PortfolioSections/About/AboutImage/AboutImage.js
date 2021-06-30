58af98937a2d473eb2bbeda7e13556cd3ef0273e
Kyle Buttress <kylebuttress11@gmail.com>
2021-06-30 15:13:41 +0200

    Configured Image component for a glitch effect on the About section

--- src/components/PortfolioSections/About/AboutImage/AboutImage.js
+++ src/components/PortfolioSections/About/AboutImage/AboutImage.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+import classes from './AboutImage.module.css';
+
+const AboutImage = () => {
+  return (
+    <div className={classes.ImageContainer}>
+      <div class={classes.ImageGlitch}></div>
+      <div class={classes.ImageGlitch}></div>
+      <div class={classes.ImageGlitch}></div>
+      <div class={classes.ImageGlitch}></div>
+      <div class={classes.ImageGlitch}></div>
+    </div>
+  )
+}
+
+export default AboutImage;
