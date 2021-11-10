f78f7b57ea97d96b895b65d8704e60ee0cf24780
Kyle <kylebuttress11@gmail.com>
2021-11-10 10:22:27 +0200

    test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>,
